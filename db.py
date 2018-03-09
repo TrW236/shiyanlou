db054bfa0721c7dee0aa0949504a733e5511e635
Tianran <ryan_236@hotmail.com>
2018-03-09 16:31:06 +0100

    corrected

--- db.py
+++ db.py
@@ -1,37 +1,14 @@
 # coding: utf-8
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
+import random
+from faker import Factory
+from sqlalchemy import create_engine, Table
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
 from sqlalchemy import Column, String, Integer, Text
-from sqlalchemy import Table
+from sqlalchemy.orm import sessionmaker, relationship
 
 engine = create_engine('mysql+mysqldb://root@localhost:3306/blog?charset=utf8')
 Base = declarative_base()
-
-article_tag = Table(
-    'article_tag', Base.metadata,
-
-    Column('article_id', Integer, ForeignKey('articles.id')),
-    Column('tag_id', Integer, ForeignKey('tags.id'))
-)
-
-class Tag(Base):
-    __tablename__ = 'tags'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(64), nullable=False, index=True)
-
-    def __repr__(self):
-        return '%s(%r)' % (self.__class__.__name__, self.name)
-
-class Category(Base):
-    __tablename__ = 'categories'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(64), nullable=False, index=True)
-    
-    def __repr__(self):
-        return '%s(%r)' % (self.__class__.__name__, self.name)
-
 
 class User(Base):
     __tablename__ = 'users'
@@ -48,8 +25,9 @@ class User(Base):
 
 class UserInfo(Base):
     __tablename__ = 'userinfos'
+
     id = Column(Integer, primary_key=True)
-    name =Column(String(64))
+    name = Column(String(64))
     qq = Column(String(11))
     phone = Column(String(11))
     link = Column(String(64))
@@ -57,22 +35,46 @@ class UserInfo(Base):
 
 class Article(Base):
     __tablename__ = 'articles'
+
     id = Column(Integer, primary_key=True)
     title = Column(String(255), nullable=False, index=True)
     content = Column(Text)
     user_id = Column(Integer, ForeignKey('users.id'))
-    author = relationship('User')
+    cate_id = Column(Integer, ForeignKey('categories.id'))
+
+    tags = relationship('Tag', secondary='article_tag', backref='articles')  # added
+    # author = relationship('User')
 
     def __repr__(self):
         return '%s(%r)' % (self.__class__.__name__, self.title)
 
+class Category(Base):
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64), nullable=False, index=True)
+    articles = relationship('Article', backref='category')  # added
+
+    def __repr__(self):
+        return '%s(%r)' % (self.__class__.__name__, self.name)
+
+article_tag = Table(
+    'article_tag', Base.metadata,
+    Column('article_id', Integer, ForeignKey('articles.id')),
+    Column('tag_id', Integer, ForeignKey('tags.id'))
+)
+
+class Tag(Base):
+    __tablename__ = 'tags'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64), nullable=False, index=True)
+
+    def __repr__(self):
+        return '%s(%r)' % (self.__class__.__name__, self.name)
 
 if __name__ == '__main__':
-    Base.metadata.create_all(engine)
 
-    from sqlalchemy.orm import sessionmaker
-    from faker import Factory
-    import random
+    Base.metadata.create_all(engine)
 
     faker = Factory.create()
     Session = sessionmaker(bind=engine)
